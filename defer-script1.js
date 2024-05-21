1665300c9985ac6c9315297b7f91c94d20a75b62
SingWang123 <gnissingtest@gmail.com>
2024-05-21 14:39:04 +0800

    測試測試

--- defer-script1.js
+++ defer-script1.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.appendChild(p3);
     
     let bigImg3 = document.createElement("img");
-    bigImg3.src = "https://png.pngtree.com/thumb_back/fw800/background/20231221/pngtree-natural-scenery-countryside-pastoral-countryside-photo-image_15541882.png";
+    bigImg3.src = "https://hips.hearstapps.com/hmg-prod/images/%E5%9C%966-%E5%9C%A8%E5%A4%AA%E5%B9%B3%E5%B1%B1%E5%9C%8B%E5%AE%B6%E6%A3%AE%E6%9E%97%E9%81%8A%E6%A8%82%E5%8D%80%E6%A3%AE%E6%9E%97%E6%AD%A5%E9%81%93-%E4%BA%AB%E5%8F%97%E6%A3%AE%E6%9E%97%E6%B5%B4%E6%B4%97%E7%A6%AE-1591677294.png?crop=1xw:1xh;center,top&resize=980:*";
     p3.appendChild(bigImg3);
 });
 
