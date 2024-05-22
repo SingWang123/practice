608bba721040c730394907ff8fcca84f16b95df0
SingWang123 <gnissingtest@gmail.com>
2024-05-22 15:21:17 +0800

    加上inline script測試

--- defer-script2.js
+++ defer-script2.js
@@ -1,5 +1,5 @@
 console.log('Defer Script 2 Executed');
-document.addEventListener("DOMContentLoaded", function() {
+
     let p4 = document.createElement('p');
     p4.textContent = 'Defer Script 2 Loaded';
     document.body.appendChild(p4);
@@ -10,5 +10,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     img4_Time = new Date();
     console.log("Defer Script 2 " + img4_Time);
-});
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let p4 = document.createElement('p');
+//     p4.textContent = 'Defer Script 2 Loaded';
+//     document.body.appendChild(p4);
+
+//     let bigImg4 = document.createElement("img");
+//     bigImg4.src = "https://www.travel.taipei/content/images/articles/377867/1024x768_admin-image-9uxix2an7kcltddfr2nt0g.png";
+//     p4.appendChild(bigImg4);
+
+//     img4_Time = new Date();
+//     console.log("Defer Script 2 " + img4_Time);
+// });
 
